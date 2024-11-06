7d4ca89df6b4f1df09898101e4170eea98d62809
mecampan <mecampan@ucsc.edu>
2024-11-05 17:18:14 -0800

    Button for L16 Dialog implemented

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-// todo
+import "./style.css";
+
+//const app: HTMLDivElement = document.querySelector("#app")!;
+const gameName = "GeoCoin Game";
+document.title = gameName;
+
+const messageButton = document.createElement("button");
+messageButton.textContent = "Push the button.";
+
+messageButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+document.body.appendChild(messageButton);
